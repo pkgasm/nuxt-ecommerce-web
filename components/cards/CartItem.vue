5ca28f7e55ea6a35881498a14deb325108d84e11
albersanmar <45761567+albersanmar@users.noreply.github.com>
2023-02-20 23:01:29 -0600

    Carrito terminado

---
+++ components/cards/CartItem.vue
@@ -0,0 +1,95 @@
+<template>
+  <v-card class="card" variant="text">
+    <div class="d-flex">
+      <div>
+        <v-img class="card__img" src="/img/image.png" cover></v-img>
+      </div>
+      <div class="d-flex flex-column">
+        <v-card-title class="w-100 d-flex align-center">
+          Nombre del producto
+        </v-card-title>
+        <v-card-subtitle class="text-left"> $1,000 </v-card-subtitle>
+        <v-card-actions class="card__quantity__desktop">
+          <v-btn variant="text" size="x-small">Eliminar</v-btn>
+        </v-card-actions>
+      </div>
+      <v-spacer></v-spacer>
+      <div
+        class="card__quantity__desktop mt-5 mx-5 d-flex flex-row align-center"
+      >
+        <v-text-field
+          v-model="quantity"
+          class="centered-input"
+          variant="outlined"
+          density="compact"
+          readonly
+          append-inner-icon="mdi-plus"
+          prepend-inner-icon="mdi-minus"
+          @click:append-inner="quantity++"
+          @click:prepend-inner="quantity--"
+        ></v-text-field>
+      </div>
+      <v-spacer></v-spacer>
+      <div class="card__total desktop mr-10 d-flex flex-row align-center">
+        <div class="text-h5">$2,500</div>
+      </div>
+    </div>
+    <div class="card__quantity__mobile d-flex flex-row justify-end">
+      <div>
+        <v-text-field
+          v-model="quantity"
+          class="centered-input"
+          variant="outlined"
+          density="compact"
+          readonly
+          append-inner-icon="mdi-plus"
+          prepend-inner-icon="mdi-minus"
+          @click:append-inner="quantity++"
+          @click:prepend-inner="quantity--"
+        ></v-text-field>
+      </div>
+      <div class="mt-1 ml-5 text-h6">$2,500</div>
+    </div>
+  </v-card>
+</template>
+<script setup>
+const quantity = ref(1);
+</script>
+<style lang="scss">
+@import "@/assets/variables.scss";
+.card {
+  width: auto !important;
+  @include screen(landscape) {
+    width: 720px !important;
+  }
+  &__img {
+    width: 100px !important;
+    height: 100%;
+    @include screen(desktop) {
+      width: 150px;
+    }
+  }
+  &__quantity {
+    &__desktop {
+      display: none !important;
+      @include screen(tablet) {
+        display: flex !important;
+      }
+    }
+    &__mobile {
+      @include screen(tablet) {
+        display: none !important;
+      }
+    }
+  }
+  &__total {
+    display: none !important;
+    @include screen(tablet) {
+      display: flex !important;
+    }
+  }
+}
+.centered-input input {
+  text-align: center;
+}
+</style>
---
+++ pages/carrito.vue
@@ -0,0 +1,76 @@
+<template>
+  <section class="cart d-flex flex-column align-center">
+    <div class="cart__total__mobile d-flex flex-column ">
+      <div class="w-100 px-14 d-flex">
+        <div class="text-h6">Total:</div>
+        <v-spacer></v-spacer>
+        <div class="text-h6">$7,500</div>
+      </div>
+      <v-btn class="mt-5 mx-14" color="primary">Continuar comprar</v-btn>
+    </div>
+    <v-card class="cart__content">
+      <div
+        class="cart__items mb-5 mt-10 d-flex flex-column align-center justify-center"
+      >
+        <cards-cart-item class="mb-3" v-for="slide in 5" :key="slide" />
+      </div>
+      <v-divider></v-divider>
+      <div class="cart__total__desktop">
+        <div class="d-flex justify-end">
+          <div class="text-h5 mr-5">Total:</div>
+          <div class="text-h5">$7,500</div>
+        </div>
+        <div class="mt-3 d-flex justify-end">
+          <v-btn color="primary">Continuar comprar</v-btn>
+        </div>
+      </div>
+    </v-card>
+  </section>
+</template>
+<style lang="scss" scope>
+@import "@/assets/variables.scss";
+.cart {
+  background-color: #f5f5f5;
+  padding-bottom: 2rem;
+  @include screen(tablet) {
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+    height: 100%;
+    margin: 1rem, 10rem, 10rem;
+  }
+  &__content {
+    margin-bottom: 7rem;
+    @include screen(landscape) {
+      width: 800px !important;
+    }
+  }
+  &__items {
+    padding-left: 2rem;
+    padding-right: 2rem;
+    @include screen(landscape) {
+      padding-right: 1rem;
+    }
+  }
+  &__total {
+    &__desktop {
+      display: none;
+      width: auto;
+      padding-right: 3rem;
+      @include screen(landscape) {
+        display: block;
+        padding-right: 0;
+        width: 720px !important;
+      }
+    }
+    &__mobile {
+      position: fixed;
+      width: 100%;
+      background-color: white;
+      z-index: 999;
+      bottom: 0;
+      padding-bottom: 1rem;
+      padding-top: 1rem;
+    }
+  }
+}
+</style>
